98bebe7b25b0835412d9f66644ca135d8b5e8530
JMirval <julien.mirval@gmail.com>
2025-06-18 14:43:46 +0200

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-import { withBlitz } from "@blitzjs/next"
+const { withBlitz } = require("@blitzjs/next")
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // your other config options
+}
 
-export default withBlitz(nextConfig)
+// Wrap with Blitz
+const rawConfig = withBlitz(nextConfig)
+
+// Remove invalid keys (Vercel-safe)
+const { __esModule, default: _default, ...sanitizedConfig } = rawConfig
+
+module.exports = sanitizedConfig
